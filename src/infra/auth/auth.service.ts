6291ce366e75bde2248f0953cc244c04d3501189
Minh Duc <mrduc0109@gmail.com>
2021-08-12 23:21:45 +0700

    feat(infra): app - add auth service and jwt strategy

---
+++ src/infra/auth/auth.service.ts
@@ -0,0 +1,87 @@
+import { compare, hash } from 'bcrypt';
+
+import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { Mapper } from '@nartc/automapper';
+import { UserEntity } from '@src/domain/user';
+import { UserRepository } from '@src/infra/user/user.repository';
+
+@Injectable()
+export class AuthService {
+    constructor(
+        private readonly userRepository: UserRepository,
+        private readonly jwtService: JwtService,
+    ) {}
+
+    /**
+     * Hash password
+     * @param {string} password
+     * @returns {Promise<string>}
+     */
+    async hashPassword(password: string): Promise<string> {
+        const hashedpassword = await hash(password, 10);
+
+        return hashedpassword;
+    }
+
+    /**
+     * Compare password and hashedPassword
+     *
+     * @param {string} password
+     * @param {string} hashedPassword
+     * @returns {Promise<string>}
+     */
+    async compareHashPassword(
+        password: string,
+        hashedPassword: string,
+    ): Promise<boolean> {
+        const valid = await compare(password, hashedPassword);
+
+        return valid;
+    }
+
+    /**
+     * Validate user authentication
+     *
+     * @param {string} email
+     * @param {string} password
+     * @returns {Promise<UserEntity | null>}
+     */
+    async validateUser(
+        email: string,
+        password: string,
+    ): Promise<UserEntity | null> {
+        const userInDb = await this.userRepository.findByEmail(email);
+
+        if (!userInDb) {
+            return null;
+        }
+
+        // validate login password
+        const isValid = await this.compareHashPassword(
+            password,
+            userInDb.hashedPassword,
+        );
+
+        if (!isValid) return null;
+
+        return Mapper.map(userInDb, UserEntity);
+    }
+
+    /**
+     * Generate token from user data
+     *
+     * @param {UserEntity} user
+     * @returns {Promise<string>} signed jwt token
+     */
+    async getToken(user: UserEntity): Promise<string> {
+        // token payload
+        const payload = {
+            email: user.email,
+            sub: user.id,
+        };
+
+        // return signed jwt token
+        return this.jwtService.sign(payload);
+    }
+}
