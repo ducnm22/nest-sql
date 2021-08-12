dc25fc093262ea8d5b4f90416ecd541ca42c54e2
Minh Duc <mrduc0109@gmail.com>
2021-08-12 23:24:56 +0700

    fix(app): user - hash password and correct data type

--- src/app/user/createUser/createUser.module.ts
+++ src/app/user/createUser/createUser.module.ts
@@ -1,10 +1,11 @@
+import { AuthModule } from '@src/infra/auth/auth.module';
 import { CreateUserUsecase } from './createUser.usecase';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserRepository } from '@src/infra/user/user.repository';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([UserRepository])],
+    imports: [TypeOrmModule.forFeature([UserRepository]), AuthModule],
     providers: [CreateUserUsecase],
     exports: [CreateUserUsecase],
 })
